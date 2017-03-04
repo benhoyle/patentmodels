6c3659ed45a1911900014d816ab06ce1f1fc79b8
benhoyle <benjhoyle@gmail.com>
2017-03-04 09:53:01 +0000

    Tidying and easy import

--- README.rst
+++ README.rst
@@ -2,3 +2,9 @@ Patent Models
 =============
 
 Functions and datamodels for patent data.
+
+Usage
+--------------
+``from patentmodels import PatentDoc, Description, Claimset, Claims, Classification``
+
+
---
+++ patentmodels/__init__.py
@@ -0,0 +1,4 @@
+from patentmodels.specification import PatentDoc, Description, Figures
+from patentmodels.claim import Claim
+from patentmodels.claimset import Claimset
+from patentmodels.classification import Classification
